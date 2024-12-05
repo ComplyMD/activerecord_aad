a199cd7c8676f53129f2fbaf4d673e19408cb32a
Patrick Connelly <patrick.connelly@nuance.com>
2024-12-05 10:01:17 -0500

    add python script to get token

--- lib/activerecord_aad/identity_manager.rb
+++ lib/activerecord_aad/identity_manager.rb
@@ -37,9 +37,11 @@ module ActiveRecordAAD
         @access_token, access_token_expires_on = fetch_access_token
 
         # TODO: validate token
-        # header, payload, signature = @access_token.split('.')
-        # decoded_payload = JSON.parse(Base64.decode64(payload))
-        # access_token_expires_on = decoded_payload['exp']
+        if access_token_expires_on.nil?
+          header, payload, signature = @access_token.split('.')
+          decoded_payload = JSON.parse(Base64.decode64(payload))
+          access_token_expires_on = decoded_payload['exp']
+        end
 
         @access_token_expires_on = Time.at(access_token_expires_on.to_i)
         @access_token_fetched_at = Time.now
@@ -102,20 +104,41 @@ module ActiveRecordAAD
     # Fetches the access token from the specified URL.
     def fetch_access_token
       logger('fetch_access_token').info('Fetching token')
+      token = nil
+      expires_on = nil
 
       begin
-        access_token_response = HTTParty.get(url, headers: { 'Metadata' => 'true' }, timeout: @properties[:timeout])
-      rescue StandardError => e
-        logger('fetch_access_token').info('Error fetching token')
-        raise "ActiveRecordAAD: unable to get access token: `#{e.message}`"
+        # raise 'Error!'
+        # response = HTTParty.get(@properties[:endpoint], query: {
+        #   api_version: @properties[:api_version],
+        #   resource: @properties[:resource],
+        #   client_id: @properties[:client_id]
+        # }, headers: {
+        #   'Metadata' => 'true'
+        # }, timeout: @properties[:timeout])
+
+        # token = response.parsed_response
+
+        response = HTTParty.get(url, headers: { 'Metadata' => 'true' }, timeout: @properties[:timeout])
+
+        unless response.success?
+          logger('fetch_access_token').info('Unsuccessful response')
+          raise "ActiveRecordAAD: unsuccessful access token request: `#{access_token_response.code} - #{access_token_response.message} - #{access_token_response.body}`"
+        end
+
+        token, expires_on = response.values_at 'access_token', 'expires_on'
+      rescue StandardError => http_error
+        logger('fetch_access_token').info("ActiveRecordAAD: error getting access token: `#{http_error.message}`")
+
+        begin
+          command = "python3 bin/get_token.py #{@properties[:client_id]}"
+          token = `#{command}`.strip
+        rescue StandardError => python_error
+          logger('fetch_access_token').info("ActiveRecordAAD: unable to get access token: `#{python_error.message}`")
+        end
       end
 
-      unless access_token_response.success?
-        logger('fetch_access_token').info('Unsuccessful response')
-        raise "ActiveRecordAAD: unable to get access token: `#{access_token_response.code} - #{access_token_response.message} - #{access_token_response.body}`"
-      end
-
-      access_token_response.values_at 'access_token', 'expires_on'
+      return token, expires_on
     end
   end
 end
